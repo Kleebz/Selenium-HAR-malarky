eaf40fc47b6276c6841bd8e6da6c95a89d0e947a
Jacob <jacobmorosoff@gmail.com>
2021-06-02 20:30:10 -0700

    Example

--- lighthouseScrape/Program.cs
+++ lighthouseScrape/Program.cs
@@ -2,6 +2,8 @@
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Remote;
 using System;
 using System.Diagnostics;
 using System.Linq;
@@ -11,42 +13,53 @@ namespace lighthouseScrape
 {
     class Program
     {
+        private static readonly string ExtensionPath = @""; //Path to browser extension which is included in cloned files, EX: C:\har_export_trigger-0.6.1-an+fx.xpi
+        private static readonly string LogDir = @""; //Set Logging Dir, EX: C:\LoggingDir
+
         static void Main(string[] args)
         {
             FirefoxOptions options = new FirefoxOptions();
-            options.AddArgument("--headless");
+            //options.AddArgument("--headless");
             options.AcceptInsecureCertificates = true;
             options.AddArgument("ignore-certificate-errors");
-            Proxy proxy = new Proxy();
-            proxy.Kind = ProxyKind.Manual;
-            proxy.IsAutoDetect = false;
-            proxy.SslProxy = "localhost:8080";
-            options.Proxy = proxy;
+            options.AddArguments("--devtools", "");
+
             options.Profile = new FirefoxProfile();
-            options.Profile.SetPreference("devtools.netmonitor.har.enableAutoExportToFile", true);
-            options.Profile.SetPreference("devtools.netmonitor.har.defaultLogDir", Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
-            options.Profile.SetPreference("devtools.netmonitor.har.defaultFileName", "network-log-file-%Y-%m-%d-%H-%M-%S");
+            options.Profile.AddExtension(ExtensionPath);
+
+            options.SetPreference("extensions.firebug.allPagesActivation", "on");
+            options.SetPreference("devtools.netmonitor.enabled", true);
+            options.SetPreference("extensions.netmonitor.har.enableAutomation", true);
+            options.SetPreference("extensions.netmonitor.har.contentAPIToken", "test");
+            options.SetPreference("extensions.netmonitor.har.autoConnect", true);
+            options.SetPreference("devtools.netmonitor.har.compress", false);
+            options.SetPreference("devtools.netmonitor.har.defaultFileName", "Autoexport_%y%m%d_%H%M%S");
+            options.SetPreference("devtools.netmonitor.har.defaultLogDir", LogDir);
+            options.SetPreference("devtools.netmonitor.har.enableAutoExportToFile", true);//If you set to false, it wont create multiple files. ToDo: Look into exportingt a single output instead of page by page.
+            options.SetPreference("devtools.netmonitor.har.forceExport", true);
+            options.SetPreference("devtools.netmonitor.har.includeResponseBodies", true);
+            options.SetPreference("devtools.netmonitor.har.jsonp", false);
+            options.SetPreference("devtools.netmonitor.har.jsonpCallback", false);
+            options.SetPreference("devtools.netmonitor.har.pageLoadedTimeout", "10000");
 
             try
             {
                 CodePagesEncodingProvider.Instance.GetEncoding(437);
                 Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-                IWebDriver driver = new FirefoxDriver(@"D:\geckodriver-v0.27.0-win64", options);
-                driver.Navigate().GoToUrl(@"https://www.selenium.dev/documentation/en/webdriver/http_proxies/");
-                Actions keyAction = new Actions(driver);
-                keyAction.KeyDown(Keys.LeftControl).KeyDown(Keys.LeftShift).SendKeys("q").KeyUp(Keys.LeftControl).KeyUp(Keys.LeftShift).Perform();
-                string url = driver.Url;
-                string source = driver.PageSource;
+                FirefoxDriver driver = new FirefoxDriver(options);
 
-                Console.WriteLine("Reached site");
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                driver.Navigate().GoToUrl("https://homestarrunner.com/toons#Teen%20Girl%20Squad");
 
+                System.Threading.Thread.Sleep(10000);
+
+                driver.Close();
                 driver.Quit();
             }
-            catch(Exception exception){
+            catch (Exception exception)
+            {
                 Console.WriteLine($"{exception.Message}\n{exception.InnerException.Message}");
             }
-
-            Console.ReadLine();
         }
     }
 }
